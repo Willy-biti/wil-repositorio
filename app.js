72a0824c728e2c61c8a26541f10fca64a2e3c025
Willy Biti <willy.biti@gmail.com>
2022-02-01 18:35:09 -0300

    Arrumando o css da página estática

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 
-app.use(express.static('./public'));
+app.use(express.static('public'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
